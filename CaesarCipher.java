db11a38484fadc75fdb8ff03aa479c07a738b80c
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2023-10-05 08:54:17 -0500

    CaesarCipher changes
    Refactored char to int and int to char conversion. Added spaces before braces. Removed blank lines. Moved documentation to precede the code it describes.

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -5,39 +5,47 @@ public class CaesarCipher{
     * Provides methods to encode and decode a word by adding/subtracting a key to it.
     */
     
-    public static void printNewWord (int num, String newWord){
-        /** 
-         * Prints out a string formatted as "n = (key) : (en/decoded word)"
-         * Input: an integer and a word
-         * Output: Returns nothing. Prints string to the screen.
-         */
-        
+    /** 
+     * Prints out a string formatted as "n = (key) : (en/decoded word)"
+     * Input: an integer and a word
+     * Output: Returns nothing. Prints string to the screen.
+     */
+    public static void printNewWord (int num, String newWord) {
         PrintWriter pen = new java.io.PrintWriter(System.out, true);
         pen.println("n = " + num + ": " + newWord);
     } //printNewWord(int, String)
- 
+
+    public static void getCypherLetter (String word, int index, int cypherType) {
+         char letter = word.charAt(index);
+         int numLetter = (int) letter;
+         if (cypherType == 1) {
+             int numNewLetter = (numLetter + n);
+             if (numNewLetter > 122) {
+                 numNewLetter -= 26;
+             }//if
+         } else if (cypherType == 2) {
+             int numNewLetter = (numLetter - n);
+             if (numNewLetter < 97){
+                 numNewLetter += 26;
+             }//if
+         }//else
+         char cypherLetter = (char) (numNewLetter);
+         return cypherLetter;
+    }//getCypherLetter
+
     
-    public static void encode(String word) throws Exception{
-        /**
+    
+     /**
          * Displays all 26 possible ways of encoding a string.
          * Input: A string.
          * Output: Returns nothing. Prints encryption results to the screen.
          */
-        
+    public static void encode(String word) throws Exception {
         int size = word.length();
         char[] letterArray = word.toCharArray();
-
-
-        for (int n = 0; n < 26; n++){
-            for (int i = 0; i < size; i++){
-                char letter = word.charAt(i);
-                int numLetter = (int) letter;
-                int numNewLetter = (numLetter + n);
-                if (numNewLetter > 122){
-                    numNewLetter -= 26;
-                }//if
-                char newLetter = (char) (numNewLetter);
-                letterArray[i] = newLetter;
+        for (int n = 0; n < 26; n++) {
+            char newLetter = getCypherLetter(word, i, 1);     
+            letterArray[i] = newLetter;
             }//for
             String encodedWord = new String (letterArray);
             printNewWord(n, encodedWord);
@@ -45,27 +53,18 @@ public class CaesarCipher{
     }//encode(String)
 
 
-
-    public static void decode(String word) throws Exception{
-        /**
-         * Displays all 26 possible ways of decoding a string.
-         * Input: A string.
-         * Output: Returns nothing. 
-         */
+    /**
+     * Displays all 26 possible ways of decoding a string.
+     * Input: A string.
+     * Output: Returns nothing. 
+     */
+    public static void decode(String word) throws Exception {
         
         int size = word.length();
         char[] letterArray = word.toCharArray();
-        
-
-        for (int n = 0; n < 26; n++){
-            for (int i = 0; i < size; i++){
-                char letter = word.charAt(i);
-                int numLetter = (int) letter;
-                int numNewLetter = (numLetter - n);
-                if (numNewLetter < 97){
-                    numNewLetter += 26;
-                }//if
-                char newLetter = (char) (numNewLetter);
+        for (int n = 0; n < 26; n++) {
+            for (int i = 0; i < size; i++) {
+                char newLetter = getCypherLetter(word, i, 2);
                 letterArray[i] = newLetter;
             }//for
             String decodedWord = new String (letterArray);
@@ -74,29 +73,23 @@ public class CaesarCipher{
     }//decode(String)
 
     
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         PrintWriter pen = new java.io.PrintWriter(System.out, true);
-        
         String error_one = "First argument should be: either encode and decode, second: a lowercase/no whitespace word";
         String error_two = "There should be two inputs";
-
         
         if (args[0].equals("encode") == false && args[0].equals("decode") == false){
             System.err.println(error_one);
             System.exit(0);
-        } else if (args.length != 2){
+        } else if (args.length != 2) {
             System.err.println(error_two);
             System.exit(0);
         } //else if
-
-        if(args[0].equals("encode")){
+        if(args[0].equals("encode")) {
             encode(args[1]);
-        } else if(args[0].equals("decode")){
+        } else if(args[0].equals("decode")) {
             decode(args[1]);
-        }//else if
-      
-
-                
+        }//else if     
     } // main(String[] args)
 } // CaesarCipher
