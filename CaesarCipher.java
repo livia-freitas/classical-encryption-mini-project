d463a6ba3a25a1ab2da84c31b2aefe8e05d11a40
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2023-10-05 18:03:17 -0500

    Add files via upload
    Formatting and edge cases

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,95 +1,96 @@
 import java.io.PrintWriter;
 
-public class CaesarCipher{
+public class CaesarCipher {
     /**
-    * Provides methods to encode and decode a word by adding/subtracting a key to it.
-    */
-    
-    /** 
-     * Prints out a string formatted as "n = (key) : (en/decoded word)"
-     * Input: an integer and a word
+     * Provides methods to encode and decode a word by adding/subtracting a key to it.
+     */
+
+    /**
+     * Prints out a string formatted as "n = (key) : (en/decoded word)" Input: an integer and a word
      * Output: Returns nothing. Prints string to the screen.
      */
-    public static void printNewWord (int num, String newWord) {
+    public static void printNewWord(int num, String newWord) {
         PrintWriter pen = new java.io.PrintWriter(System.out, true);
         pen.println("n = " + num + ": " + newWord);
-    } //printNewWord(int, String)
+    } // printNewWord(int, String)
 
-    public static void getCypherLetter (String word, int index, int cypherType) {
-         char letter = word.charAt(index);
-         int numLetter = (int) letter;
-         if (cypherType == 1) {
-             int numNewLetter = (numLetter + n);
-             if (numNewLetter > 122) {
-                 numNewLetter -= 26;
-             }//if
-         } else if (cypherType == 2) {
-             int numNewLetter = (numLetter - n);
-             if (numNewLetter < 97){
-                 numNewLetter += 26;
-             }//if
-         }//else
-         char cypherLetter = (char) (numNewLetter);
-         return cypherLetter;
-    }//getCypherLetter
+    /**
+     * Takes in a word, an index, a loop number, and a number (1 or 2) representing encode/decode.
+     * Returns the cypher letter.
+     */
+    public static char getCypherLetter(String word, int index, int loopNum, int cypherType) {
+        char letter = word.charAt(index);
+        int numLetter = (int) letter;
+        int numNewLetter = -1;
+        int alphabetSize = 26;
+        int alphabetUpperBound = 122;
+        int alphabetLowerBound = 97;
 
-    
-    
-     /**
-         * Displays all 26 possible ways of encoding a string.
-         * Input: A string.
-         * Output: Returns nothing. Prints encryption results to the screen.
-         */
+        if (cypherType == 1) { // cypherType 1 is encode
+            numNewLetter = numLetter + loopNum;
+            if (numNewLetter > alphabetUpperBound) {
+                numNewLetter -= alphabetSize;
+            } // if
+        } else if (cypherType == 2) { // cypherType 2 is decode
+            numNewLetter = numLetter - loopNum;
+            if (numNewLetter < alphabetLowerBound) {
+                numNewLetter += alphabetSize;
+            } // if
+        } // else
+        char cypherLetter = (char) numNewLetter;
+        return cypherLetter;
+    }// getCypherLetter
+
+    /**
+     * Displays all 26 possible ways of encoding a string. Input: A string. Output: Returns nothing.
+     * Prints encryption results to the screen.
+     */
     public static void encode(String word) throws Exception {
         int size = word.length();
         char[] letterArray = word.toCharArray();
         for (int n = 0; n < 26; n++) {
-            char newLetter = getCypherLetter(word, i, 1);     
-            letterArray[i] = newLetter;
-            }//for
-            String encodedWord = new String (letterArray);
+            for (int i = 0; i < size; i++) {
+                char newLetter = getCypherLetter(word, i, n, 1);
+                letterArray[i] = newLetter;
+            } // for
+            String encodedWord = new String(letterArray);
             printNewWord(n, encodedWord);
-        }//for
-    }//encode(String)
-
+        } // for
+    }// encode(String)
 
     /**
-     * Displays all 26 possible ways of decoding a string.
-     * Input: A string.
-     * Output: Returns nothing. 
+     * Displays all 26 possible ways of decoding a string. Input: A string. Output: Returns nothing.
      */
     public static void decode(String word) throws Exception {
-        
+
         int size = word.length();
         char[] letterArray = word.toCharArray();
         for (int n = 0; n < 26; n++) {
             for (int i = 0; i < size; i++) {
-                char newLetter = getCypherLetter(word, i, 2);
+                char newLetter = getCypherLetter(word, i, n, 2);
                 letterArray[i] = newLetter;
-            }//for
-            String decodedWord = new String (letterArray);
+            } // for
+            String decodedWord = new String(letterArray);
             printNewWord(n, decodedWord);
-        }//for
-    }//decode(String)
+        }
+    }// decode(String)
 
-    
     public static void main(String[] args) throws Exception {
 
         PrintWriter pen = new java.io.PrintWriter(System.out, true);
-        String error_one = "First argument should be: either encode and decode, second: a lowercase/no whitespace word";
-        String error_two = "There should be two inputs";
-        
-        if (args[0].equals("encode") == false && args[0].equals("decode") == false){
-            System.err.println(error_one);
+
+        if (args.length != 2) {
+            System.err.println("There should be exactly two inputs. Please try");
             System.exit(0);
-        } else if (args.length != 2) {
-            System.err.println(error_two);
+        } else if (args[0].equals("encode") == false && args[0].equals("decode") == false) {
+            System.err.println(
+                    "First argument should be: either encode and decode, second: a lowercase/no whitespace word.");
             System.exit(0);
-        } //else if
-        if(args[0].equals("encode")) {
+        } // else if
+        if (args[0].equals("encode")) {
             encode(args[1]);
-        } else if(args[0].equals("decode")) {
+        } else if (args[0].equals("decode")) {
             decode(args[1]);
-        }//else if     
+        } // else if
     } // main(String[] args)
 } // CaesarCipher
--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,114 +1,127 @@
 import java.io.PrintWriter;
 
-public class VigenereCipher{
+public class VigenereCipher {
 
-  
-    public static String wordRepeat(String plainText, String keyword){
     /**
-    * Returns a new word (the size of the plain text word) made of the repeated keyword.
-    * Input: Two strings, a plain text string and the word to be repeated.
-    * Output: A string.
-    */
+     * Returns a new word (the size of the plain text word) made of the repeated keyword. Input: Two
+     * strings, a plain text string and the word to be repeated. Output: A string.
+     */
+    public static String wordRepeat(String plainText, String keyword) {
 
         int keySize = keyword.length();
         int plainTextSize = plainText.length();
-        char[] newWordArray = plainText.toCharArray(); //to get an array the size of the plaintext word
-        for (int i = 0; i < plainTextSize; i++){
+        char[] newWordArray = plainText.toCharArray(); // to get an array the size of the plaintext
+                                                       // word
+        for (int i = 0; i < plainTextSize; i++) {
             int modIndex = i % keySize;
             char currentLetter = keyword.charAt(modIndex);
             newWordArray[i] = currentLetter;
-        }//for
-        String newWord = new String (newWordArray);
+        } // for
+        String newWord = new String(newWordArray);
         return newWord;
-    }//for
+    }// for
 
-    
-    public static void encode(String plainText, String keyword){
     /**
-    * Shifts a word by adding the alphabetical value of each letter of a keyword.
-    * Input: Two strings, a plain text string and the keyword.
-    * Output: Returns nothing. Prints encryption results to the screen.
-    */
+     * Takes in a plain text word, a keyword, an index number, and a number (1 or 2) representing
+     * encode/decode. Returns the cypher letter.
+     */
+    public static char getCypherLetter(String plainText, String keyword, int index,
+            int cypherType) {
+
+        char plainLetter = plainText.charAt(index);
+        char keyLetter = keyword.charAt(index);
+        int numPlainLetter = (int) plainLetter;
+        int numKeyLetter = (int) keyLetter - 97;
+        int numNewLetter = -1;
+        int alphabetSize = 26;
+        int alphabetUpperBound = 122;
+        int alphabetLowerBound = 97;
+
+        if (cypherType == 1) { // cypherType 1 is encode
+            numNewLetter = numPlainLetter + numKeyLetter;
+            if (numNewLetter > alphabetUpperBound) {
+                numNewLetter -= alphabetSize;
+            } // if
+        } else if (cypherType == 2) { // cypherType 2 is decode
+            numNewLetter = numPlainLetter - numKeyLetter;
+            if (numNewLetter < alphabetLowerBound) {
+                numNewLetter += alphabetSize;
+            } // if
+        } // else
+        char cypherLetter = (char) numNewLetter;
+        return cypherLetter;
+    }// getCypherLetter
+
+    /**
+     * Shifts a word by adding the alphabetical value of each letter of a keyword. Input: Two
+     * strings, a plain text string and the keyword. Output: Returns nothing. Prints encryption
+     * results to the screen.
+     */
+    public static void encode(String plainText, String keyword) {
+        keyword = wordRepeat(plainText, keyword);
+        PrintWriter pen = new PrintWriter(System.out, true);
+        int size = plainText.length();
+        char[] cypherArray = plainText.toCharArray();
+
+        if(keyword.equals("")) {
+            pen.println(plainText);
+        } else if (plainText.equals("")) {
+            pen.println(keyword);
+        } else {
+            for (int i = 0; i < size; i++) {
+                char newLetter = getCypherLetter(plainText, keyword, i, 1);
+                cypherArray[i] = newLetter;
+            } // for
+            String encodedWord = new String(cypherArray);
+            pen.println(encodedWord);
+        }//else
+    }// encode(String, String)
+
+    /**
+     * Shifts a word by subtracting the alphabetical value of each letter of a keyword. Input: Two
+     * strings, a plain text string and the keyword. Output: Returns nothing. Prints encryption
+     * results to the screen.
+     */
+    public static void decode(String plainText, String keyword) {
 
         keyword = wordRepeat(plainText, keyword);
         PrintWriter pen = new PrintWriter(System.out, true);
         int size = plainText.length();
         char[] cypherArray = plainText.toCharArray();
-        pen.println(keyword);
-       
-        
-        for (int i = 0; i < size; i++){
-            char plainLetter = plainText.charAt(i);
-            char keyLetter = keyword.charAt(i);
-            int numPlainLetter = (int) plainLetter;
-            int numKeyLetter = (int) keyLetter - 97;
-            int numNewLetter = (numPlainLetter + numKeyLetter);
-            if (numNewLetter > 122){
-                numNewLetter -= 26;
-            }//if
-            char newLetter = (char) (numNewLetter);
+
+        for (int i = 0; i < size; i++) {
+            char newLetter = getCypherLetter(plainText, keyword, i, 2);
             cypherArray[i] = newLetter;
-        }//for
-        String encodedWord = new String (cypherArray);
-        pen.println("cypher: " + encodedWord);
-    }//encode(String, String)
+        } // for
+        String decodedWord = new String(cypherArray);
+        pen.println(decodedWord);
+    }// decode(String, String)
 
-     public static void decode(String plainText, String keyword){
-    /**
-    * Shifts a word by subtracting the alphabetical value of each letter of a keyword.
-    * Input: Two strings, a plain text string and the keyword.
-    * Output: Returns nothing. Prints encryption results to the screen.
-    */
-
-        keyword = wordRepeat(plainText, keyword);
-        PrintWriter pen = new PrintWriter(System.out, true);
-        int size = plainText.length();
-        char[] cypherArray = plainText.toCharArray();
-
-        for (int i = 0; i < size; i++){
-            char plainLetter = plainText.charAt(i);
-            char keyLetter = keyword.charAt(i);
-            int numPlainLetter = (int) plainLetter;
-            int numKeyLetter = (int) keyLetter - 97;
-            int numNewLetter = (numPlainLetter - numKeyLetter);
-            if (numNewLetter < 97){
-                numNewLetter += 26;
-            }//if
-            
-            char newLetter = (char) (numNewLetter);
-            cypherArray[i] = newLetter;
-        }//for
-        String decodedWord = new String (cypherArray);
-        pen.println("cypher: " + decodedWord);
-     }//decode(String, String)
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         PrintWriter pen = new java.io.PrintWriter(System.out, true);
-        
-        String error_one = "First argument should be either encode and decode, and plaintext/keyword should be lowercase with no whitespaces.";
-        String error_two = "There should be three inputs: encode/decode, a word to be processed, and a keyword";
 
-        
-        if (args[0].equals("encode") == false && args[0].equals("decode") == false){
+        String error_one =
+                "First argument should be either encode and decode, and plaintext/keyword should be lowercase with no whitespaces.";
+        String error_two =
+                "There should be three inputs: encode/decode, a word to be processed, and a keyword";
+
+        if (args.length != 3) {
+            System.err.println(error_two); //!!!!!!!
+            System.exit(0);
+        } else if (args[0].equals("encode") == false && args[0].equals("decode") == false) {
             System.err.println(error_one);
             System.exit(0);
-        }else if (args.length != 3){
-            System.err.println(error_two);
-            System.exit(0);
-        }//else if
+        } 
 
 
-        if(args[0].equals("encode")){
+        if (args[0].equals("encode")) {
             encode(args[1], args[2]);
-        } else if(args[0].equals("decode")){
+        } else if (args[0].equals("decode")) {
             decode(args[1], args[2]);
-        }//else if
-   
-    }//main(String[])
-}//VigenereCipher
-        
+        } // else if
+
+    }// main(String[])
+}// VigenereCipher
 
 
-        
-  
